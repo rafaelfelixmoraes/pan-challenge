0a43da7c7ebac9e74239fff19ebd9983afbc1346
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-03-13 17:16:49 -0300

    - Commit Inicial: Criação do Projeto

---
+++ pan-challenge/src/main/java/com/rafaelfelix/panchallenge/PanChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelfelix.panchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanChallengeApplication.class, args);
+	}
+
+}
